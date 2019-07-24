97aa0fe733bd6e27070305ae2bbfba14b5a3be2b
Henrique Urban Pessoa <titepessoa@gmail.com>
2019-07-24 12:33:14 -0300

    Created my random integer generator.

--- ReflexGame/Form1.cs
+++ ReflexGame/Form1.cs
@@ -10,6 +10,30 @@ using System.Windows.Forms;
 
 namespace ReflexGame
 {
+    class MyRandomGenerator
+    {
+        readonly bool isFake;
+        Random trueGenerator;
+
+        public MyRandomGenerator(bool isFake = false)
+        {
+            this.isFake = isFake;
+            trueGenerator = new Random();
+        }
+
+        public int NextInt(int start, int limit, int fakeResult = 0)
+        {
+            if (isFake)
+            {
+                return fakeResult;
+            }
+            else
+            {
+                return trueGenerator.Next(start, limit);
+            }
+        }
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,14 +41,14 @@ namespace ReflexGame
             InitializeComponent();
         }
 
-        private Rectangle GetRandomSquare()
+        private Rectangle GetRandomSquare(MyRandomGenerator rnd)
         {
-
+            int squareSize = rnd.NextInt(10, 100);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Rectangle rect = GetRandomSquare();
+            Rectangle rect = GetRandomSquare(new MyRandomGenerator());
             Pen pen = new Pen(Color.Aqua, 2);
             e.Graphics.DrawEllipse(pen, rect);
         }
